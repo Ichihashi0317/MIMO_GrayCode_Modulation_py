14232fda7e80f27cc87d1ad75b29126ae7347c65
Ichihashi0317 <103499611+Ichihashi0317@users.noreply.github.com>
2023-07-31 23:48:53 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import math
+import numpy as np
+from Modulator import Modulator
+
+def main():
+    # パラメータ設定
+    M = 16          # アンテナ本数
+    Q_ant = 64      # 1アンテナあたりの多値数
+    SNR_dB = 30     # SNR [dB]
+    K = 1024        # シンボル長
+    nloops = 100    # ループ数
+
+    # 1送信ベクトルあたりのビット長
+    q = int(math.log2(Q_ant)) * M
+
+    # 雑音レベル計算
+    SNR = 10.0 ** (SNR_dB / 10.0)
+    N0 = M / SNR
+    sigma_noise = math.sqrt(N0 / 2.0)
+    
+    # 変調器インスタンス生成
+    mod = Modulator(Q_ant)
+
+    # ビットエラー数
+    BE = 0
+
+    # テストループ
+    for _ in range(nloops):
+
+        # 送信ビット生成
+        bits = np.random.randint(2, size=[q, K])
+
+        # 変調
+        X = mod.modulate(bits)
+
+        # AWGN通信路
+        Z = np.random.normal(0.0, sigma_noise, size=[M, K]) + 1j * np.random.normal(0.0, sigma_noise, size=[M, K])
+        Y = X + Z
+
+        # 復調
+        bits_hat = mod.demodulate(Y)
+
+        # ビットエラー数計算
+        BE += (bits != bits_hat).sum()
+    
+    # ビット誤り率計算
+    BER = BE / nloops / K / q
+
+    # 結果表示
+    print('BER = ', BER)
+
+if __name__ == '__main__':
+    main()
