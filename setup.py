392e7f10d30f0261f88f69ba610a753fbf915c62
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-09-18 13:59:47 +0900

    Add install_requires

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MIMO_GrayCode_Modulation',
-    version='1.0',
-    packages=find_packages()
+    name="MIMO_GrayCode_Modulation",
+    version="1.0",
+    packages=find_packages(),
+    install_requires=["numpy"],
 )
