3d37760129b655a6e2c3a90d28d87cda61ec2e39
clcl777 <77223796+clcl777@users.noreply.github.com>
2023-09-18 13:26:25 +0900

    example修正

--- example.py
+++ example.py
@@ -1,16 +1,16 @@
 import math
 import numpy as np
-from modulator import Modulator
+from MIMO_GrayCode_Modulation_py.modulator import Modulator
 
 
 def main():
     # パラメータ設定
-    M = 16          # アンテナ本数
-    Q = 64          # 1アンテナあたりの多値数
-    K = 1024        # シンボル長
-    SNR_dB = 20     # SNR [dB]
-    nloops = 100    # ループ数
-    
+    M = 16  # アンテナ本数
+    Q = 64  # 1アンテナあたりの多値数
+    K = 1024  # シンボル長
+    SNR_dB = 20  # SNR [dB]
+    nloops = 100  # ループ数
+
     # 変復調器インスタンス生成
     mod = Modulator(Q)  # 複素モデル
     # mod = Modulator(Q, complex=False)   # 実数等価モデル
@@ -19,11 +19,10 @@ def main():
     SNR = 10.0 ** (SNR_dB / 10.0)
     N0 = 1.0 / SNR  # アンテナ間で干渉がない場合
     sigma_noise = math.sqrt(N0 / 2.0)
-    
+
     # テストループ
     BE = 0
     for _ in range(nloops):
-
         # 送信ビット列生成
         bits = mod.gen_bits(M, K)
 
@@ -31,7 +30,9 @@ def main():
         X = mod.modulate(bits)
 
         # AWGN通信路
-        Z = np.random.normal(0.0, sigma_noise, size=[M, K]) + 1j * np.random.normal(0.0, sigma_noise, size=[M, K])  # 複素モデル
+        Z = np.random.normal(0.0, sigma_noise, size=[M, K]) + 1j * np.random.normal(
+            0.0, sigma_noise, size=[M, K]
+        )  # 複素モデル
         # Z = np.random.normal(0.0, sigma_noise, size=[2*M, K])   # 実数等価モデル
         Y = X + Z
 
@@ -40,13 +41,13 @@ def main():
 
         # ビットエラー数計算
         BE += (bits != bits_hat).sum()
-    
+
     # ビット誤り率計算
     BER = BE / (nloops * K * M * math.log2(Q))
 
     # 結果表示
-    print('BER =', BER)
+    print("BER =", BER)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
