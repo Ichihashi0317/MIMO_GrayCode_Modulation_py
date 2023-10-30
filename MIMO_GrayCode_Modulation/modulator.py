a0155fd6ad2006726161bfdc656fbe59f515015f
Ichihashi0317 <103499611+Ichihashi0317@users.noreply.github.com>
2023-10-31 01:46:42 +0900

    Update modulator.py

--- MIMO_GrayCode_Modulation/modulator.py
+++ MIMO_GrayCode_Modulation/modulator.py
@@ -23,9 +23,7 @@ class Modulator:
             self.k_demod = np.sqrt(
                 2 * np.mean((label_dim - self.mean) ** 2)
             )  # Coefficient to set symbol spacing to 1.
-            self.k_mod = (
-                1 / self.k_demod
-            )  # Coefficient to set power per dimension to 1/2.
+            self.k_mod = 1 / self.k_demod  # Coefficient to set power per dimension to 1/2.
         elif Q_ant > 4:
             # For QAM.
             Q_dim = int(math.sqrt(Q_ant))  # Modulation level per dimension.
@@ -83,9 +81,8 @@ class Modulator:
                 syms = syms.astype(complex)
             else:
                 M, K = syms.shape
-                syms = np.concatenate(
-                    [syms, np.zeros([M, K])], axis=0
-                )  # Because zeros is a real number type, syms is output as a real number type
+                syms = np.concatenate([syms, np.zeros([M, K])], axis=0)
+                # Because zeros is a real number type, syms is output as a real number type
         elif self.Q_ant == 4:
             # QPSK
             # Generate symbols
@@ -141,17 +138,9 @@ class Modulator:
             M = syms.shape[0] // 2
             K = syms.shape[1]
             # Normalization/integer (multiply by a factor to obtain an integer interval, add a constant so that it is greater than or equal to zero, and round to the nearest integer)
-            syms_ = (
-                np.around(syms * self.k_demod + self.mean)
-                .astype(int)
-                .clip(0, self.Q_dim - 1)
-            )
+            syms_ = np.around(syms * self.k_demod + self.mean).astype(int).clip(0, self.Q_dim - 1)
             # Calculate bit
-            bits = (
-                self.bittab_dim_[:, syms_]
-                .transpose(1, 0, 2)
-                .reshape(2 * M * self.q_dim, K)
-            )
+            bits = self.bittab_dim_[:, syms_].transpose(1, 0, 2).reshape(2 * M * self.q_dim, K)
         return bits
 
     @staticmethod
